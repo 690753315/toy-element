92b748ef0da243b4ad2d3d1e9199f882b7fe87c9
ymslx-2018 <690753315@qq.com>
2024-10-11 13:17:59 +0800

    feat: 06 项目打包分包

--- packages/core/vite.es.config.ts
+++ packages/core/vite.es.config.ts
@@ -3,6 +3,20 @@ import vue from "@vitejs/plugin-vue"
 import dts from "vite-plugin-dts"
 import { resolve } from "path"
 
+import { readdirSync, readdir } from "fs"
+
+import { filter, map, includes } from "lodash-es"
+
+// 获取basePath下所有文件夹名组成的数组
+function getDirectoriesSync(basePath: string) {
+  const entries = readdirSync(basePath, { withFileTypes: true })
+
+  return map(
+    filter(entries, (entry) => entry.isDirectory()),
+    (entry) => entry.name
+  )
+}
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -36,6 +50,21 @@ export default defineConfig({
           return assetInfo.name !== "style.css"
             ? (assetInfo.name as string)
             : "index.css"
+        },
+        manualChunks(id) {
+          if (includes(id, "node_modules")) return "vendor"
+
+          if (includes(id, "/packages/hooks")) return "hooks"
+
+          if (
+            includes(id, "/packages/utils") ||
+            includes(id, "plugin-vue:export-helper")
+          )
+            return "utils" // 如果是components下的文件夹，以每个文件夹的名字为单独一个包
+
+          for (const item of getDirectoriesSync("../components")) {
+            if (includes(id, `/packages/components/${item}`)) return item
+          }
         }
       }
     }
